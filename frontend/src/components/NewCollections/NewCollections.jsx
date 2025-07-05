db2b778bf6cf1aed31118c24e9ecedc12319ca1e
abhilashmoturi <moturiabhilash@gmail.com>
2025-07-05 18:02:55 +0530

    url fixed

--- frontend/src/components/NewCollections/NewCollections.jsx
+++ frontend/src/components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collections, setNewcollection] = React.useState([]);
 
   useEffect(()=>{
-    fetch('https://ecommerce-wc28.onrender.com//newcollections')
+    fetch('https://ecommerce-wc28.onrender.com/newcollections')
     .then((res)=>res.json())
     .then((data)=>setNewcollection(data));
   },[])
